5f504ae1484149bc97e5b37faf6ba42e5daf1617
ramyaagowri123 <121948984+ramyaagowri123@users.noreply.github.com>
2023-02-06 22:20:10 +0530

    Add files via upload

---
+++ PRATICE/Objects.js
@@ -0,0 +1,25 @@
+//Object entries
+
+const obj = { 10: 'arry', 21: 'barry', 23: 'carry' };  
+console.log(Object.entries(obj)[0]);
+
+// Object.keys()
+
+const object1 = {
+    a: 'somestring',
+    b: 42,
+    c: false
+  };
+  
+  console.log(Object.keys(object1));
+
+  //Object.values
+
+  const object2 = {
+    a: 'somestring',
+    b: 42,
+    c: false
+  };
+  
+  console.log(Object.values(object2));
+
